63225c2f5fcc6695c6049fe12f1bbec683e328cd
José Riobueno <jrrv99@gmail.com>
2024-11-08 02:15:46 -0400

    feat(Proyecto_1): add communities feature

--- Proyecto_1/src/Main.kt
+++ Proyecto_1/src/Main.kt
@@ -14,6 +14,61 @@ fun getVecinos(g: Grafo, u: Int): List<Int> { // Obtener la lista de vecinos de 
     return g.adyacentes(u).map { it.elOtroVertice(u) }.sorted()
 }
 
+fun getMatrizDeAdyacencia(g: Grafo): Matrix {
+    var n = g.obtenerNumeroDeVertices()
+    var A = Matrix(n,n) // el constructor por defecto inicializa en ceros
+    val esNoDirigido = g is GrafoNoDirigido || g is GrafoNoDirigidoCosto
+    for(i in 1..n){
+        for(j in getVecinos(g,i)){
+            A.set(i-1,j-1,1.0)
+            if(esNoDirigido) A.set(j-1,i-1,1.0)
+        }
+    }
+    return A
+}
+
+fun getMatrizDeAlcance(A: Matrix): Matrix {
+    val R = A.copy()
+    val n = R.rowDimension
+    for (k in 0 until n) {
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                if (R.get(i, j) == 0.0 && R.get(i, k) == 1.0 && R.get(k, j) == 1.0) {
+                    R.set(i, j, 1.0)
+                }
+            }
+        }
+    }
+    return R
+}
+
+fun getComponentesConexas(g: Grafo): List<List<Int>> { //Construir una lista de lista de los rótulos (Int) de los nodos con las componentes conexas
+    val n = g.obtenerNumeroDeVertices()
+    val A = getMatrizDeAdyacencia(g) // construir la matriz de adyacencia (A)
+    val R = getMatrizDeAlcance(A) // construir la matriz de alcance (R)
+
+    // Utilizar la matriz C (copia de R) para extraer las componentes conexas
+    val C = R.copy()
+    val esNoDirigido = g is GrafoNoDirigido || g is GrafoNoDirigidoCosto
+    val visitado = BooleanArray(n) // El constructor por defecto inicializa en False
+    val componentes = mutableListOf<List<Int>>()
+    for (i in 0 until n) {
+        C.set(i,i,1.0)
+        if (!visitado[i]) {
+            var componente = mutableListOf<Int>()
+            for (j in 0 until n) {
+                val criterio = if(esNoDirigido) (C.get(i,j) == 1.0 && !visitado[j]) else (C.get(i,j) == 1.0 && C.get(j,i) == 1.0)
+                if (criterio) {
+                    componente.add(j+1) // OJO, la matrices comienzan en cero mientras los grafos en uno
+                    visitado[j] = true
+                }
+            }
+            componentes.add(componente)
+        }
+    }
+    return componentes
+}
+
 /**
  * Definitions
  */
@@ -75,10 +130,32 @@ class ILoveCatsNetwork(
 
     fun printWinners(winners: List<User>, category: String) {
         println("\tUSUARIOS CON $category AMIGOS=${winners.size}")
-
+        
         for ((index, user) in winners.withIndex()) {
             println("\t\t${index+1}:${user.id}:${user.friends.size}:${user.friends}")
         }
+    }
+
+    fun getCommunities(): List<List<Int>> {
+        val communities = getComponentesConexas(this.friendsGraph)
+
+        return communities.sortedByDescending { it.size }
+    }
+
+    fun printCommunities() {
+        val communities = this.getCommunities()
+        println("\tCOMUNIDADES DE AMIGOS=${communities.size}")
+
+        for ((c_index, community) in communities.withIndex()) {
+            println("\t\tCOMUNIDAD ${c_index + 1}")
+
+            for ((u_index, user_id) in community.withIndex()) {
+                val user: User = this.users[user_id] as User
+
+                println("\t\t\t${u_index + 1}:${user.id}:${user.friends.size}:${user.friends}")
+            }
+        }
+
     }
 
     fun report() {
@@ -89,6 +166,8 @@ class ILoveCatsNetwork(
 
         this.printWinners(usersWithMoreFriends, "MAS")
         this.printWinners(usersWithLessFriends, "MENOS")
+
+        this.printCommunities()
     }
 }
 
